3f3f56cd187c4ea7dc7b24f56ab8ad3f63998048
mdfatehulalam <mdfatehulalam@gmail.com>
2025-09-08 17:49:22 +0600

    Time: 0 ms (100%), Space: 20.5 MB (83.38%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // most optimized method which is using bitwise operator(XOR) and its tc is O(n)
         int n = 0;     
         for(int i : nums){
             n = n ^ i;
