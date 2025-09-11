e57de1e6d4967e2b0560e9bcb96e766e555b03e6
mdfatehulalam <mdfatehulalam@gmail.com>
2025-09-11 19:54:34 +0600

    Time: 0 ms (100%), Space: 18.1 MB (20.28%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,45 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            if(target==nums[i]) return true;
-        } return false;
-        
+        int st = 0, end = nums.size() - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[st] == nums[mid] && nums[end] == nums[mid]) {
+                st++;
+                end--;
+            }
+            else if (nums[st] <= nums[mid]) {
+                if (target >= nums[st] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    st = mid + 1;
+            }
+            else {
+                if (target > nums[mid] && target <= nums[end])
+                    st = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return false;
     }
 };
+
+
+        
+
+
+
+
+
+// class Solution {
+// public:
+//     bool search(vector<int>& nums, int target) {
+//         for(int i=0; i<nums.size(); i++){
+//             if(target==nums[i]) return true;
+//         } return false;
+        
+//     }
+// };
