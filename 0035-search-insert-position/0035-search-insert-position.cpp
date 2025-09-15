308a48b6c3012f47380b1f90f8f8b099effe4993
mdfatehulalam <mdfatehulalam@gmail.com>
2025-09-15 14:13:22 +0600

    Time: 0 ms (100%), Space: 13.4 MB (99.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int searchInsert(vector<int>& arr, int target) {
         // its tc O(logn)
-        int st = 0; int end = arr.size()-1;
+        int n = arr.size();
+        int st = 0; int end = n-1;
         while(st<=end){
             int mid = st + (end-st)/2;
             if(arr[mid]==target) return mid;
