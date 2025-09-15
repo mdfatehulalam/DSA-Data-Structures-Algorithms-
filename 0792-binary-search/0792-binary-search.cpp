143f5002494b7783ed4e485b1f516343a579b844
mdfatehulalam <mdfatehulalam@gmail.com>
2025-09-15 13:46:42 +0600

    Time: 0 ms (100%), Space: 31.2 MB (98.38%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int search(vector<int>& arr, int target) {
         // its tc is O(logn)
-        int n = arr.size();
-        int st = 0; int end = n-1;
+        int st = 0; int end = arr.size()-1;
         while(st<=end){
             int mid = st + (end-st)/2;
             if(arr[mid]==target) return mid;
